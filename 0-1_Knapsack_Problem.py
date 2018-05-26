8129e8bd21c2bc7fc904fabb6f70a06878924748
Tony Scapardine <arscapa@gmail.com>
2018-05-26 14:19:47 -0500

    Completed Knapsack Algorithm

--- 0-1_Knapsack_Problem.py
+++ 0-1_Knapsack_Problem.py
@@ -1,25 +1,24 @@
 ## Given weights and values of n items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. 
 
 wt = [1,3,4,5]
-val  [1,4,5,7]
+val = [1,4,5,7]
 W = 7             ## Total capacity of knapsack
 
-T = [0 for wts in wt][0 for j in range(0,W+1)]
+T = [[0 for wts in range(W+1)] for vals in range(len(val)+1)]
+print T
 
-
-for i in range (0,len(wt)+1):
-		for x in range(0,W+1):
-			## Base cases
-			if i == 0:
-				T[i][x] = 0
-			if x == 0:
-				T[i][x] = 0
-			## Fill out table 
-			elif (wt[i] <= x):
-				T[i][x] = max(val[i] + T[i-1][x-wt[i]], T[i-1][x])
-			else:
-				T[i][x] = T[i-1][x]
-	print T
-return T[len(wt)][W]
-	
+for i in range (len(wt)+1):
+        for x in range(W+1):
+            ## Base cases
+            if i == 0 or x == 0:
+                T[i][x] = 0
+                
+            ## Fill out table 
+            elif (wt[i-1] <= x):
+                T[i][x] = max(val[i-1] + T[i-1][x-wt[i-1]], T[i-1][x])
+            else:
+                T[i][x] = T[i-1][x]
+print(T)                
+print T[len(wt)][W]
+    
 
