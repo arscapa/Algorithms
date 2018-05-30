2dc650df877393a04c37ae3dca3d9e27e80a281d
Tony Scapardine <arscapa@gmail.com>
2018-05-29 23:01:50 -0500

    Updated code to return items used to attain max value in addition to maximum value attainable

--- 0-1_Knapsack_Problem-with_Repetition.py
+++ 0-1_Knapsack_Problem-with_Repetition.py
@@ -2,24 +2,27 @@
 
 ## Given weights and values of n items, put these items in a knapsack of capacity W to get the maximum total value in the knapsack. 
 ## Items can be used multiple times 
+import copy
 
 wt = [1,3,4,5]
 val = [1,5,5,7]
 W = 7             ## Total capacity of knapsack
 
 T = [0 for x in range(W+1)]
-
-
-T[0] = 0
+items = [[] for x in range(W+1)]
 
 for w in range(1,W+1):
     for i in range(len(val)):
         if wt[i] <= w and T[w] < val[i] + T[w-wt[i]]:
             T[w] = val[i] + T[w-wt[i]]
-   #print ("At weight capacity of "+str(w)+" max attainable value = "+str(T[w]))
+            temp = copy.copy(items[w-wt[i]])
+            temp.append(val[i])
+            items[w] = temp
+   
 print(T)
-print("\n Maximum value attainable with weight capacity of " + str(W) + " is value " + str(T[w]))
-    
+print(items)
+print("\n Maximum value attainable with weight capacity of " + str(W) + " is value " + str(T[W]))
+print("\n ...attained using items with values " + str(items[W]))    
 
 
 	
