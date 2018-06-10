6221d147d8b33b84e1b4443749720d65498709ec
Tony Scapardine <arscapa@gmail.com>
2018-06-10 14:49:47 -0500

    Re-formatted algorithm as a function

--- 0-1_Knapsack_Problem.py
+++ 0-1_Knapsack_Problem.py
@@ -2,38 +2,45 @@
 ## Items can only be used once (no-repetition)
 import copy
 
-wt = [2,3,4,6]
-val = [9,14,16,30]
-W = 10             ## Total capacity of knapsack
-
-T = [[0 for wts in range(W+1)] for vals in range(len(val)+1)]
-nums = [[ [] for wts in range(W+1)] for vals in range(len(val)+1)]
+def knapsack(wt,val,W):
+        T = [[0 for wts in range(W+1)] for vals in range(len(val)+1)]
+        nums = [[ [] for wts in range(W+1)] for vals in range(len(val)+1)]
 
 
-for i in range (len(wt)+1):
-        for x in range(W+1):
-            ## Base cases
-            if i == 0 or x == 0:
-                T[i][x] = 0
+        for i in range (len(wt)+1):
+                for x in range(W+1):
+                    ## Base cases
+                    if i == 0 or x == 0:
+                        T[i][x] = 0
                 
-            ## Fill out table 
-            elif (wt[i-1] <= x) and (val[i-1] + T[i-1][x-wt[i-1]] > T[i-1][x]):
-                T[i][x] = val[i-1] + T[i-1][x-wt[i-1]] ## update max value
+                    ## Fill out table 
+                    elif (wt[i-1] <= x) and (val[i-1] + T[i-1][x-wt[i-1]] > T[i-1][x]):
+                        T[i][x] = val[i-1] + T[i-1][x-wt[i-1]] ## update max value
 
-                temp = copy.copy(nums[i-1][x-wt[i-1]])
-                temp.append(val[i-1])
-                nums[i][x]= temp                      ## update knapsack contents                                                
+                        temp = copy.copy(nums[i-1][x-wt[i-1]])
+                        temp.append(val[i-1])
+                        nums[i][x]= temp                      ## update knapsack contents                                                
                 
-            else:
-                T[i][x] = T[i-1][x]
+                    else:
+                        T[i][x] = T[i-1][x]
 
-                temp = copy.copy(nums[i-1][x])
-                nums[i][x] = temp
+                        temp = copy.copy(nums[i-1][x])
+                        nums[i][x] = temp
                 
-print len(wt),"items \n", "with weights:", "\n", wt,"\n\n","and values:", "\n", val,"\n"
-print "Knapsack capacity = ",W, "\n"
+        print len(wt),"items \n", "with weights:", "\n", wt,"\n\n","and values:", "\n", val,"\n"
+        print "Knapsack capacity = ",W, "\n"
 
-print "Maxiumum achievable value is", T[len(wt)][W]
-print "Knapsack consists of items with values", nums[len(wt)][W]
+        print "Maxiumum achievable value is", T[len(wt)][W]
+        print "Knapsack consists of items with values", nums[len(wt)][W]
+        return T[len(wt)][W]
+
+
+
+
+weights = [2,3,4,6]
+values = [9,14,16,30]
+maxW = 10             ## Total capacity of knapsack
+
+knapsack(weights,values,maxW)
     
 
