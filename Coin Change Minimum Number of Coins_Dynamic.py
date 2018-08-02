b548b34a12fb418122b9930e26d523df754080cf
Tony Scapardine <arscapa@gmail.com>
2018-08-01 19:21:21 -0500

    Updated algorithm to return denominations of coins necessary to make change

--- Coin Change Minimum Number of Coins_Dynamic.py
+++ Coin Change Minimum Number of Coins_Dynamic.py
@@ -1,22 +1,28 @@
 ## Given a total and coins of values specified, determine least number of coins necessary to make change
 ## Assume an unlimited supply of coins of each denomination
+
 import copy
 
 
 def coin_change(coin,total):
-        ## Initialize empty array to store values
+        ## Initialize empty array to store coint count necessary to make change
         T = [[0 for x in range(total+1)] for y in range(len(coin))]
-        print T
-        ## Build table from left to right, top down
+        ## To store coin denominations necessary to make change
+        change = [[ [] for x in range(total+1)] for y in range(len(coin))]
+
+        
+        ## Build table from left to right
         for val in range(len(coin)):
                 for subtotal in range(1,total+1):
-                        print "current coin is",val, " with value of ", coin[val]
-                        print "current subtotal",subtotal   
+                        # print "current coin is",val, " with value of ", coin[val]
+                        # print "current subtotal",subtotal   
                     
                         ## If subtotal equals coin value exactly, can make change with single coin
                         if subtotal == coin[val]:
-                                print "Setting current subtotal for coin = 1"
                                 T[val][subtotal] = 1
+                                ## Add coin denomination to change necessary
+                                change[val][subtotal] = [coin[val]]
+                  
 
 
                         ## If total is greater than coin value and you're able to make change with the leftover total amount after subtracting coin value..       
@@ -25,24 +31,46 @@ def coin_change(coin,total):
                         elif subtotal > coin[val] and T[val][subtotal-coin[val]] <> 0:
                                 ## Compare prior solution only if was able to make change 
                                 if T[val-1][subtotal] > 0 :
-                                        print "Compare prior solution and take min "
+                                        ## Update coin count
                                         T[val][subtotal] = min(1+T[val][subtotal-coin[val]],T[val-1][subtotal])
+
+                                        ## update change necessary
+                                        if T[val][subtotal] == (1+T[val][subtotal-coin[val]]):
+                                                temp = copy.copy(change[val][subtotal-coin[val]])
+                                                temp.append(coin[val])
+                                                change[val][subtotal] = temp
+                                        else:
+                                                change[val][subtotal] = change[val-1][subtotal]
+                                                
+                                                
                                 else:
                                         ## Was unable to make change with previous coins only, take only possible solution
                                         T[val][subtotal] = 1+T[val][subtotal-coin[val]]
 
+                                        ## Update change necesasry
+                                        temp = copy.copy(change[val][subtotal-coin[val]])
+                                        temp.append(coin[val])
+                                        change[val][subtotal] = temp
+
                         else:
-                                print "else condition"
                                 T[val][subtotal] = T[val-1][subtotal]
-                        print T
-                        
-        return T
+
+                                ## update change necessary
+                                change[val][subtotal] = change[val-1][subtotal]
+                                
+                
+
+        return T[len(coin)-1][total], change[len(coin)-1][total]
 
 
 coin_input = [2,5,10]
-total_input = 9
+total_input = 31
 
 
-print coin_change(coin_input,total_input)
-    
+answer = coin_change(coin_input,total_input)
+
+print "Given unlimited coins with denominations ",coin_input
+print "You can make change for",total_input
+print "with a minimum of",answer[0], "coins"
+print "Using coin denominations ",answer[1]
 
